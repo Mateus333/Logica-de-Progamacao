75aeb183d6bbd5b0a4a65e844fb35c923784e43c
Mateus Sobreira <mateussobreira80@gmail.com>
2025-02-06 18:26:48 -0300

    Exercicio 1 de logica de programação

---
+++ Exercicio1.js
@@ -0,0 +1,13 @@
+function identificarParOuImpar(numero) {
+    if (numero % 2 === 0) {
+        console.log('O número informado é par');
+    } else {
+        console.log('O número informado é ímpar')
+    }
+}
+
+identificarParOuImpar(5); //Ímpar
+identificarParOuImpar(10); //Par
+identificarParOuImpar(2); //Par
+identificarParOuImpar(1); //Ímpar
+identificarParOuImpar(0); //Par
